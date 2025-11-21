28654292ed76843a13c3a347c34ad9d2cabfdb36
Ryan Williams <idontwantyourspamthanks@gmail.com>
2025-11-21 11:40:42 +0000

    Highlight search-relevant bits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def highlight_query(text, query)
+    return text unless query.present? && text.present?
+
+    highlight(text, query, highlighter: '<mark>\1</mark>')
+  end
 end
