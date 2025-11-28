e059d6fe19d06a5d6dc52e883018daca1faade1e
Ryan Williams <idontwantyourspamthanks@gmail.com>
2025-11-28 23:27:49 +0000

    Fix broken cache-bust

--- app/javascript/controllers/image_bust_controller.js
+++ app/javascript/controllers/image_bust_controller.js
@@ -5,20 +5,23 @@ export default class extends Controller {
 
   connect() {
     this.busted = false
-    // If the controller is attached directly to the image element, set target manually
-    if (!this.hasImageTarget && this.element.tagName === "IMG") {
-      this.imageTarget = this.element
-    }
   }
 
   retry() {
     if (this.busted) return
-    if (!this.imageTarget) return
+    const image = this.currentImage()
+    if (!image) return
 
     this.busted = true
-    const src = this.imageTarget.src
+    const src = image.src
     const separator = src.includes("?") ? "&" : "?"
     const bustedSrc = `${src}${separator}bustCache=${Date.now()}`
-    this.imageTarget.src = bustedSrc
+    image.src = bustedSrc
+  }
+
+  currentImage() {
+    if (this.hasImageTarget) return this.imageTarget
+    if (this.element.tagName === "IMG") return this.element
+    return null
   }
 }
